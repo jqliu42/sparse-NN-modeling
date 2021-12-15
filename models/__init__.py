043cea2f0072a597c5dda1a3ef1a0cf9e3099d71
jqliu42 <jqliu42@mail.ustc.edu.cn>
2021-12-15 16:36:54 +0800

    first commit

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .shufflenet import *
+from .shufflenetv2 import *
+from .resnet import *
+from .resnext import *
+from .mobilenet import *
+from .mobilenetv2 import *
+from .efficientnet import *
